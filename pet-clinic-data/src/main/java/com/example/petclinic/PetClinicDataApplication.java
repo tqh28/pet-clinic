1a398a4b83156376e6fe6a4b4fe5a2bd76ebf1d2
Huy Tran <tqh28>
2018-12-16 10:45:49 +0700

    Create multi-module project for data model. Close #1

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/PetClinicDataApplication.java
@@ -0,0 +1,12 @@
+package com.example.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicDataApplication.class, args);
+	}
+}
--- pet-clinic-web/src/main/java/com/example/petclinic/PetClinicWebApplication.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/PetClinicWebApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetClinicApplication {
+public class PetClinicWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, args);
+		SpringApplication.run(PetClinicWebApplication.class, args);
 	}
 }
