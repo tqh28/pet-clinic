e64f26c8258f0a269df5c38895f114560f481aac
Huy Tran <tqh28>
2019-01-02 10:10:06 +0700

    add base entity contain id field. close #10

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.example.petclinic.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+	public Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
